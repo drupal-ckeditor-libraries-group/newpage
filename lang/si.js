ca8a889694cfd1bd17deda1f9951c24407dc425f
Wiktor Walc <w.walc@cksource.com>
2013-06-10 13:22:00 +0200

    Updated translations, added Sinhala language.

--- lang/si.js
+++ lang/si.js
@@ -2,6 +2,6 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'newpage', 'gl', {
-	toolbar: 'Páxina nova'
+CKEDITOR.plugins.setLang( 'newpage', 'si', {
+	toolbar: 'නව පිටුවක්'
 });
